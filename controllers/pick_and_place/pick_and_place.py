3fc63766456397da1c299b0e7979a8f9197677ea
Adele <chindahadele2@gmail.com>
2022-09-01 02:10:31 +0300

    Optimization functions implemented

--- controllers/pick_and_place/pick_and_place.py
+++ controllers/pick_and_place/pick_and_place.py
@@ -8,7 +8,7 @@ robot = Robot()
 # Get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
 
-# Inizialize base motors.
+#! Inizialize base motors.
 wheels = []
 wheels.append(robot.getDevice("wheel1"))
 wheels.append(robot.getDevice("wheel2"))
@@ -20,7 +20,7 @@ for wheel in wheels:
     # and setVelocity will set the maximum motor velocity instead of the target velocity.
     wheel.setPosition(float('+inf'))
 
-# Initialize arm motors.
+#! Initialize arm motors.
 armMotors = []
 armMotors.append(robot.getDevice("arm1"))
 armMotors.append(robot.getDevice("arm2"))
@@ -28,12 +28,13 @@ armMotors.append(robot.getDevice("arm3"))
 armMotors.append(robot.getDevice("arm4"))
 armMotors.append(robot.getDevice("arm5"))
 # Set the maximum motor velocity.
-armMotors[0].setVelocity(0.2)
-armMotors[1].setVelocity(0.5)
-armMotors[2].setVelocity(0.5)
-armMotors[3].setVelocity(0.3)
+armMotors[0].setVelocity(1.5) # maxVelocity = 1.5
+armMotors[1].setVelocity(1.5)
+armMotors[2].setVelocity(1.5)
+armMotors[3].setVelocity(0.5)
+armMotors[4].setVelocity(1.5)
 
-# Initialize arm position sensors.
+#! Initialize arm position sensors.
 # These sensors can be used to get the current joint position and monitor the joint movements.
 armPositionSensors = []
 armPositionSensors.append(robot.getDevice("arm1sensor"))
@@ -44,32 +45,86 @@ armPositionSensors.append(robot.getDevice("arm5sensor"))
 for sensor in armPositionSensors:
     sensor.enable(timestep)
 
-# Initialize gripper motors.
+#! Initialize gripper motors.
 finger1 = robot.getDevice("finger1")
 finger2 = robot.getDevice("finger2")
 # Set the maximum motor velocity.
-finger1.setVelocity(0.03)
-finger2.setVelocity(0.03)
+finger1.setVelocity(1.5)
+finger2.setVelocity(1.5) # 0.03
 # Read the miminum and maximum position of the gripper motors.
 fingerMinPosition = finger1.getMinPosition()
 fingerMaxPosition = finger1.getMaxPosition()
 
-# Move forward.
-for wheel in wheels:
-    wheel.setVelocity(7.0)
-# Wait until the robot is in front of the box.
-robot.step(520 * timestep)
+#! Generic forward motion function
+def forward():
+    for wheel in wheels:
+        wheel.setVelocity(7.0) # maxVelocity = 14.81
 
-# Stop moving forward.
-for wheel in wheels:
-    wheel.setVelocity(0.0)
+#! Generic stop function
+def halt():
+    for wheel in wheels:
+        wheel.setVelocity(0.0)
 
+def fold_arms():
+    armMotors[0].setPosition(-2.9)
+    armMotors[1].setPosition(1.5)
+    armMotors[2].setPosition(-2.6)
+    armMotors[3].setPosition(1.7)
+    armMotors[4].setPosition(0)
+
+def stretch_arms():
+    armMotors[0].setPosition(2.9)
+    armMotors[1].setPosition(-1.0)
+    armMotors[2].setPosition(2.5)
+    armMotors[3].setPosition(-1.7)
+    armMotors[4].setPosition(0)
+
+def turn_around():
+#TODO: robot.step(70 * timestep) - produces a 90 degrees turn
+    wheels[0].setVelocity(14)
+    wheels[1].setVelocity(-14)
+    wheels[2].setVelocity(14)
+    wheels[3].setVelocity(-14)
+    robot.step(70 * timestep)
+    # forward()
+
+def pick_up():
+    armMotors[1].setPosition(-0.55)
+    armMotors[2].setPosition(-0.9)
+    armMotors[3].setPosition(-1.5)
+    finger1.setPosition(fingerMaxPosition)
+    finger2.setPosition(fingerMaxPosition)
+
+#TODO: youBot default position:
+#TODO: x: -2.4; y: 0; z: 0.101
+#TODO: robot.step(70 * timestep) - produces a 90 degrees turn
 # Move arm and open gripper.
-armMotors[1].setPosition(-0.55)
-armMotors[2].setPosition(-0.9)
-armMotors[3].setPosition(-1.5)
-finger1.setPosition(fingerMaxPosition)
-finger2.setPosition(fingerMaxPosition)
+#? arm[0] maxPosition = 2.9 || -2.9
+#? arm[1] maxPosition = 1.5 || -1.0
+#? arm[2] maxPosition = 2.5 || -2.6
+#? arm[3] maxPosition = 1.7 || -1.7
+#? arm[4] maxPosition = 2.9 || -2.9
+# x = True
+# while x: 
+#     # halt()
+#     turn_around()
+#     robot.step(210 * timestep)
+#     halt()
+#     stretch_arms()
+#     robot.step(200 * timestep)
+#     fold_arms()
+#     robot.step(200 * timestep)
+#     # forward()
+#     # robot.step(100 * timestep)
+
+#! Functions call start here
+forward()
+robot.step(520 * timestep)# Wait until the robot is in front of the box.
+halt()
+pick_up()
+
+
+
 
 # Monitor the arm joint position to detect when the motion is completed.
 while robot.step(timestep) != -1:
@@ -87,14 +142,21 @@ robot.step(50 * timestep)
 armMotors[1].setPosition(0)
 # Wait until the arm is lifted.
 robot.step(200 * timestep)
-
+# turn_around()
 # Rotate the robot.
-wheels[0].setVelocity(2.5)
-wheels[1].setVelocity(-2.5)
-wheels[2].setVelocity(2.5)
-wheels[3].setVelocity(-2.5)
+wheels[0].setVelocity(7)
+wheels[1].setVelocity(-7)
+wheels[2].setVelocity(7)
+wheels[3].setVelocity(-7)
 # Wait for a fixed amount to step that the robot rotates.
-robot.step(690 * timestep)
+robot.step(280 * timestep)
+# robot.step(6900 * timestep)
+
+#! -------------------------
+#! PICK UP FROM HERE
+#! -------------------------
+
+# robot.step(690 * timestep)
 
 # Move forward.
 wheels[1].setVelocity(2.5)
